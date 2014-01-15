6e02548042ebff6cd64dc28607c8e760502d6020
LuckyPunk <moutafis@tem.uoc.gr>
2014-01-15 19:32:09 +0200

    cloud

---
+++ cloudbox.c
@@ -0,0 +1,164 @@
+#include <stdlib.h>
+#include <stdio.h> 
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <>dirent.h>
+#include "cloudbox.h"
+
+/*
+ * The list that holds all the current watched files.
+ * 
+ * It is very convinient this list to be shorted by the file name
+ * in order to be able to find immediatly inconsistencies,
+ */
+struct dir_files_status_list *watched_files;
+
+/*
+ * Print mutex, for printing nicely the messages from different threads
+ */
+pthread_mutex_t print_mutex;
+
+
+/* 
+ * Mutex used to protect the accesses from different threads
+ * of the file list of the watched directory
+ */
+pthread_mutex_t file_list_mutex;
+
+
+
+
+int main(int argc, char **argv){
+	int opt;
+	int scan_interval;
+	int broadcast_port;
+	
+	char *client_name;
+	char *watched_dir;
+	watched_files=NULL;
+	
+	/*
+	 * Initialize the mutexes
+	 */
+	pthread_mutex_init(&print_mutex, NULL);
+	pthread_mutex_init(&file_list_mutex, NULL);
+	
+	while ((opt = getopt(argc, argv, "hn:d:i:b:")) != -1) {
+		switch(opt){
+			case 'n':
+				client_name = strdup(optarg);
+				break;
+				
+			case 'd':
+				watched_dir = strdup(optarg);
+				/* A few checks will be nice here...*/
+				/* Convert the given dir to absolute path */
+				break;
+			case 'i':
+				scan_interval = atoi(optarg);
+				break;
+			case 'b':
+				broadcast_port = atoi(optarg);
+				/* To check or not to check? */
+				break;
+			default:
+				printf("Usage: cloudbox -n client_name -d directory_to_use -i scan_interval -b broadcast_port\n"
+				"Options:\n"
+				"   -n                  Specifies the name of the client\n"
+				"   -d                  The directory absolute path, to watch for changes\n"
+				"   -i                  The interval time in seconds, that the client should scan for file changes\n"
+				"   -b                  The port that is going to be used for receiving and transmitting broadcasts UDP meesages\n"
+				"   -h                  prints this help\n");
+				exit(EXIT_FAILURE);
+		}
+	}
+	
+	printf("Cloudbox client %s:\n"
+		   "Wathced directory: %s\n"
+		   "Scan interval: %d seconds\n"
+		   "Broadcast port: %d\n",
+		client_name, watched_dir, scan_interval, broadcast_port);
+	watched_files=insert_file(watched_files,watched_dir,10,"sha",36210);
+	watched_files=insert_file(watched_files,"myfile2",20,"sha",33210);
+	watched_files=insert_file(watched_files,"myfile2",20,"sha",33210);
+	watched_files=delete_file(watched_files,"myfile2");
+	watched_files=delete_file(watched_files,"C:/jagathan");
+	while(watched_files){
+		printf("\n%s ",watched_files->filename);
+		watched_files=watched_files->next;
+	}
+
+	return 0;
+}
+struct dir_files_status_list* insert_file(struct dir_files_status_list *head,char *filename ,size_t size_in_bytes ,char sha1sum[SHA1_BYTES_LEN],
+                                        long long int modifictation_time_from_epoch){
+
+        struct dir_files_status_list *cur ,*newnode,*prev;
+        prev=NULL;
+        cur=head;
+        while((cur)&&(strcmp(filename,cur->filename)>0)){
+                prev=cur;
+                cur=cur->next;
+        }
+        if((cur)&&(strcmp(cur->filename,filename)==0))
+                return head;
+
+        newnode=(struct dir_files_status_list*)malloc(sizeof(struct dir_files_status_list));
+        newnode->filename=(char*)malloc(sizeof(char*));
+        strcpy(newnode->filename,filename);
+        strcpy(newnode->sha1sum,sha1sum);
+        newnode->size_in_bytes=size_in_bytes;
+        newnode->modifictation_time_from_epoch=modifictation_time_from_epoch;
+        newnode->next=cur;
+        newnode->previous=prev;
+        if((!cur)&&(!prev)){
+                head=newnode;
+                return head;
+        }
+        if(!cur){
+                prev->next=newnode;
+                return head;
+        }
+        if(!prev)
+               head=newnode;
+        else{
+               prev->next=newnode;
+               cur->previous=newnode;
+        }
+        return head;
+}
+
+struct dir_files_status_list* delete_file(struct dir_files_status_list *head,char *filename){
+			struct dir_files_status_list *cur,*prev,*next;
+			cur=head;
+	        prev=NULL;
+	        while((cur)&&(strcmp(cur->filename,filename)!=0)){
+	                prev=cur;
+	                cur=cur->next;
+	        }
+	        if(!cur)
+	                return head;
+	        if(!prev){
+
+	                prev=cur;
+	                cur=cur->next;
+	                if(cur)
+	                	cur->previous=NULL;
+	                free(prev->filename);
+	                free(prev);
+	                return cur;
+	        }
+	        if(!cur->next){
+	                prev->next=NULL;
+	                free(cur->filename);
+	                free(cur);
+	                return head;
+	        }
+	        prev->next=cur->next;
+	        next=cur->next;
+	        next->previous=prev;
+	        free(cur->filename);
+	        free(cur);
+	        return head;
+}
